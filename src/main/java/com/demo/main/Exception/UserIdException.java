f038fa25994471e177cf6472cc992b9082b7b0c7
parasuramgour <parasurammal216@gmail.com>
2024-09-03 06:33:25 -1100

    Added (#1)

---
+++ src/main/java/com/demo/main/Exception/UserIdException.java
@@ -0,0 +1,15 @@
+package com.demo.main.Exception;
+
+public class UserIdException  extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UserIdException(String msg) {
+		
+		super(msg);
+	}
+
+}
